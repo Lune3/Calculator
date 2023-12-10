59ee652e8cf7a3a5a625e23d997bf0a9cd8849e0
Lune3 <113820374+Lune3@users.noreply.github.com>
2023-12-10 21:42:33 +0530

    Got number to be displayed on screen

--- javascript.js
+++ javascript.js
@@ -3,11 +3,20 @@ function buttonInput(){
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
         button.addEventListener("click",(e) =>{
-            
+            if(e.target.textContent != "AC" && e.target.textContent != "C" && e.target.textContent != "="){
+            screenUpdater(e.target.textContent);
+            }
+            else{
+                
+            }
         })
     });  
 }
 
+function screenUpdater(userInput){
+    let screen = document.querySelector(".userInput");
+    screen.textContent = screen.textContent.concat(userInput);
+}
 
 function main(){
     buttonInput();
