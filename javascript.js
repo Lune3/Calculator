242a284bae4e0eb20b50141a07d19dd534aa074e
Lune3 <113820374+Lune3@users.noreply.github.com>
2023-12-10 22:19:13 +0530

    Getting checkSize to work properly
    Implementing a Function to decrease size of screen font when it overflow

--- javascript.js
+++ javascript.js
@@ -5,12 +5,21 @@ function buttonInput(){
         button.addEventListener("click",(e) =>{
             if(e.target.textContent != "AC" && e.target.textContent != "C" && e.target.textContent != "="){
             screenUpdater(e.target.textContent);
+            checkSize();
             }
             else{
-                
+
             }
         })
     });  
+}
+
+function checkSize(){
+    let screen = document.querySelector(".userInput");
+    if(screen.offsetWidth > 300){
+        let sizeOfFont = screen.style.fontSize;
+        screen.style.fontSize = `${sizeOfFont - (parseInt(screen.offsetWidth) - 300)}px`;
+    }
 }
 
 function screenUpdater(userInput){
